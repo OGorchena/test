d65f47ac151b9651ba4250f66dd28b1996a82210
OGorchena <msogorchena@gmail.com>
2025-07-24 15:26:02 +0500

    xd

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from typing import Union
+data_fruts = [ "banana", "cherry", "elderberry"]
+data_vegetables = ["carrot", "broccoli", "spinach", "potato"]
 
-from fastapi import FastAPI
 
-    
-    
+if not "apple" in data_fruts:
+   for fruit in data_fruts:
+      input(f"Выбирете другой фрукт:{data_fruts}")
+      if fruit == "banana":
+         print("Вы выбрали банан")
+      elif fruit == "cherry":
+         print("Вы выбрали вишню")
+         break
+      elif fruit == "elderberry":
+         print("Вы выбрали бузину")
+         break
+else: input("Фрукт уже выбран нажмите, чтобы продолжить")
+      
+
