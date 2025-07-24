695f7aafc6f7e7312b3b92d5350d2fb1920e4d39
OGorchena <msogorchena@gmail.com>
2025-07-24 16:38:26 +0500

    Магазин

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-data_fruts = [ "banana", "cherry", "elderberry"]
+data_fruits = ["banana", "cherry", "elderberry"]
 data_vegetables = ["carrot", "broccoli", "spinach", "potato"]
 
+def choose_1():
+   index = int(input(f"Выберите один фрукт (используя цифры): {', '.join([f'{i}: {fruit}' for i, fruit in enumerate(data_fruits)])}: "))
+   if 0 <= index < len(data_fruits):
+       print(f"Вы выбрали: {data_fruits[index]}")
+   else:
+       print("Неверный индекс. Пожалуйста, попробуйте снова.")
+def choose_2():
+   index = int(input(f"Выберите один овощь (используя цифры): {', '.join([f'{i}: {fruit}' for i, fruit in enumerate(data_vegetables)])}: "))
+   if 0 <= index < len(data_vegetables):
+       print(f"Вы выбрали: {data_vegetables[index]}")
+       print(f"Спасибо за покупки! \n Ваша корзина: {data_fruits[index]}, {data_vegetables[index]}")
+   else:
+       print("Неверный индекс. Пожалуйста, попробуйте снова.")
+   
 
-if not "apple" in data_fruts:
-   for fruit in data_fruts:
-      input(f"Выбирете другой фрукт:{data_fruts}")
-      if fruit == "banana":
-         print("Вы выбрали банан")
-      elif fruit == "cherry":
-         print("Вы выбрали вишню")
-         break
-      elif fruit == "elderberry":
-         print("Вы выбрали бузину")
-         break
-else: input("Фрукт уже выбран нажмите, чтобы продолжить")
-      
 
+choose_1()
+choose_2()
