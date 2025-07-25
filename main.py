b23626dc8b07c29b5ae6661d50b6aaf03ad39905
OGorchena <msogorchena@gmail.com>
2025-07-25 13:57:16 +0500

    Update

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ class Restaurant:
    def __init__(self, restaurant_name, cuisine_type):
       self.restaurant_name = restaurant_name
       self.cuisine_type = cuisine_type
+      self.number_served = 1000
    def describe_restaurant(self):
       print(f"Название ресторана: {self.restaurant_name}")
       print(f"Тип кухни: {self.cuisine_type}")
+      
+      
+   def served_write(self):
+      print(f"Количество обслуженных посетителей: {self.number_served}")
    
    def open_restaurant(self):
       print(f"Restaurant {self.restaurant_name} open")
@@ -12,13 +17,16 @@ class Restaurant:
 my_restaurant = Restaurant("Vivaldi", "European kitchen")
 my_restaurant.describe_restaurant()
 my_restaurant.open_restaurant()
+my_restaurant.served_write()
 
 your_restaurant = Restaurant("Clood Mone", "French kitchen")
 your_restaurant.describe_restaurant()
 your_restaurant.open_restaurant()
+your_restaurant.served_write()
 
 our_restautant = Restaurant("Fishka", "Russian kitchen")
 our_restautant.describe_restaurant()
 our_restautant.open_restaurant()
+our_restautant.served_write()
 
 #dd
